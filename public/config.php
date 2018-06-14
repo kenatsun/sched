336bc1bdb4d313d22c7bc55b1d24506cc62eae27
Ken Winter <ken@sunward.org>
2018-06-14 21:22:38 +0000

    Extend deadline for Summer 2018 survey

--- public/config.php
+++ public/config.php
@@ -11,7 +11,7 @@ date_default_timezone_get('America/Detroit');
 define('COMMUNITY', 'Sunward');
 
 /* -------- seasonal config --------- */
-define('DEADLINE', strtotime('June 16, 2018, 11pm'));
+define('DEADLINE', strtotime('June 19, 2018, 11pm'));
 
 /* ----------- job ids --------------- */
 // define('MEETING_NIGHT_ORDERER', 4194);

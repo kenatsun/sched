edff64cd044f52623efe6923118b85fc72eb9c5b
Ken Winter <ken@sunward.org>
2018-09-14 17:17:12 +0000

    Fix bug that failed to call create_sqlite_connection()

--- public/utils.php
+++ public/utils.php
@@ -2,6 +2,33 @@
 require_once 'constants.inc';
 require_once 'git_ignored.php';
 
+function create_sqlite_connection() {
+	global $dbh;
+	global $db_is_writable;
+	$db_is_writable = FALSE;
+
+	// connect to SQLite database
+	try {
+		global $relative_dir;
+		if (!isset($relative_dir)) { 
+			$relative_dir = '';
+		}
+		else {
+			$relative_dir .= '/';
+		}
+
+		$db_fullpath = getDatabaseFullpath();  // This function is in git_ignored.php because production & development use different databases
+		$db_is_writable = is_writable($db_fullpath);
+		$db_file = "sqlite:{$db_fullpath}";
+		$dbh = new PDO($db_file);
+		$timeout = 5; // in seconds
+		$dbh->setAttribute(PDO::ATTR_TIMEOUT, $timeout);
+	}
+	catch(PDOException $e) {
+		echo "problem loading sqlite file [$db_fullpath]: {$e->getMessage()}\n";
+		exit;
+	}
+}
 
 
 function meal_date_sort($a, $b) {

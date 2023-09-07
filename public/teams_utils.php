4315046786f76aa9545427535d5a59ab11809767
kenatsun <ken@sunward.org>
2023-09-07 20:38:48 +0000

    Hide breadcrumbs & controls in printable schedule display

--- public/teams_utils.php
+++ public/teams_utils.php
@@ -39,7 +39,7 @@ function displaySchedule($controls_display="show", $change_markers_display="show
 	$crumbs = $edition ? "" : CRUMBS_QUERY;  // Omit breadcrumbs from printable editions
 	if (0) deb("teams_utils.php.displaySchedule(): crumbs = " . $crumbs);
 	if (0) deb("teams_utils.php.displaySchedule(): before renderHeadline()");
-	$headline = renderHeadline($adjective . "Sunward Dinner Teams for {$season['name']}", $subhead, 1);
+	$headline = renderHeadline($adjective . "Sunward Dinner Teams for {$season['name']}", $subhead, 0, 0);
 	if ($change_requests_line)	$change_requests_line = '<br><p style="color:blue; font-size:larger"><strong>' . $change_requests_line . '<a href="mailto:moremeals@sunward.org">moremeals@sunward.org</a></strong></p><br>'; 
 	if (0) deb("teams_utils.php.displaySchedule(): before renderAssignmentsForm()" . since("before renderAssignmentsForm()"));
 	$assignments_form = renderAssignmentsForm($controls_display, $change_markers_display, $edition);
